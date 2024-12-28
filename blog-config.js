51019f911c18b35ee2957a264cf341165570e9e4
GRIN-96 <bnmnil96@gmail.com>
2024-12-29 00:30:18 +0900

    init

---
+++ blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "grin.blog",
+  description: "안녕하세요, 적응왕 백앤드 개발자 최민기입니다.",
+  author: "Grin",
+  siteUrl: "https://grin.blog",
+  links: {
+    github: "https://github.com/GRIN-96",
+    email: "mailto:bnmnil96@gmail.com",
+  },
+  useAbout: true,
+  // See https://giscus.app/
+  giscus: {
+    repo: "GRIN-96/blog-comment",
+    repoId: "R_kgDONjOtmg",
+    category: "General",
+    categoryId: "DIC_kwDONjOtms4ClklM",
+    mapping: "pathname",
+    strict: "0",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "en",
+  },
+}
