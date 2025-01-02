8f5ad50383479c6ca36d9bb1cfabf4c201b68a45
GRIN-96 <bnmnil96@gmail.com>
2025-01-02 17:25:20 +0900

    Style: og-image 인식되도록 수정

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -3,12 +3,13 @@ import { Helmet } from "react-helmet"
 import { siteUrl } from "../../../blog-config"
 
 const SEO = ({ title, description, url }) => {
+  const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={`${siteUrl}/og-image.png?v=${today}`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
